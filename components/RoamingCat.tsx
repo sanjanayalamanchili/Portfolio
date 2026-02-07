7e703f2a6ea17fb3fb40c83cbfa86edee00de83b
sanjanayalamanchili <143543873+sanjanayalamanchili@users.noreply.github.com>
2026-02-06 20:37:43 -0600

    Create RoamingCat.tsx

---
+++ components/RoamingCat.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RoamingCat = () => {
+  return (
+    <div style={{ position: 'absolute', bottom: 0, left: 0 }}>
+      🐈 {/* Placeholder roaming cat */}
+    </div>
+  );
+};
+
+export default RoamingCat;
